209b6b3f32f27644b42eb6433878492d4a30c843
Keshav Bhardwaj <keshav_bhardwaj@hcltech.com>
2025-11-26 15:54:20 +0530

    edited files

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -1,6 +1,6 @@
-
+# Get current tenant from the logged-in context
 data "azurerm_client_config" "current" {}
-
+ 
 resource "azurerm_key_vault" "this" {
   name                        = var.key_vault_name
   location                    = var.location
@@ -9,12 +9,17 @@ resource "azurerm_key_vault" "this" {
   sku_name                    = "standard"
   purge_protection_enabled    = true
   soft_delete_retention_days  = 7
+  enabled_for_deployment      = false
+  enabled_for_disk_encryption = false
+  enabled_for_template_deployment = false
+ 
   public_network_access_enabled = false
-
+ 
   network_acls {
     bypass         = "None"
     default_action = "Deny"
   }
-
+ 
   tags = var.tags
 }
+ 
--- modules/key_vault/variables.tf
+++ modules/key_vault/variables.tf
@@ -2,3 +2,4 @@ variable "key_vault_name" { type = string }
 variable "resource_group_name" { type = string }
 variable "location" { type = string }
 variable "tags" { type = map(string) }
+ 
