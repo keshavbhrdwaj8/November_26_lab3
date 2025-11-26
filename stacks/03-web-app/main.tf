aaca5bda1cbd2c6cd9aa2613486b2a098bed8e0d
Keshav Bhardwaj <keshav_bhardwaj@hcltech.com>
2025-11-26 16:17:26 +0530

    edited files

--- stacks/03-web-app/main.tf
+++ stacks/03-web-app/main.tf
@@ -1,15 +1,3 @@
-
-# Reference Stage 2 remote state to get the Service Plan ID
-data "terraform_remote_state" "app_plan" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "Keshav_Bhardwaj_RG"
-    storage_account_name = "keshavlab2storage"
-    container_name       = "tfstate"
-    key                  = "lab2/02-app-plan.tfstate"
-  }
-}
-
 module "web_app" {
   source              = "../../modules/web_app"
   web_app_name        = var.web_app_name
